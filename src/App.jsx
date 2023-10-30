3aeac02f3b3ca1ee4c5e5b025183f50f13c814eb
Bryan Moreira <49196485+bryanmoreira@users.noreply.github.com>
2023-10-30 15:32:41 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import styled from "styled-components"
+import GlobalStyles from "./components/GlobalStyles"
+import Cabecalho from "./components/Cabecalho"
+import BarraLateral from "./components/BarraLateral"
+import Banner from "./components/Banner"
+import Galeria from "./components/Galeria"
+import fotos from './fotos.json';
+import { useState } from "react"
+import ModalZoom from "./components/ModalZoom"
+import Rodape from "./components/Rodape"
+
+const FundoGradiente = styled.div`
+    background: var(--Gradiente-fundo, linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%));
+    width: 100%;
+    min-height: 100vh;
+`
+
+const AppContainer = styled.div`
+    width: 1440px;
+    max-width: 100%;
+    margin: 0 auto;
+`
+
+const MainContainer = styled.main`
+    display: flex;
+    gap: 24px;
+`
+
+const ConteudoGaleria = styled.section`
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+`
+
+const App = () => {
+    const [fotosDaGaleria, setFotosDaGaleria] = useState(fotos)
+    const [fotoSelecionada, setFotoSelecionada] = useState(null)
+
+    const aoAlternarFavorito = (foto) => {
+        if (foto.id === fotoSelecionada?.id) {
+            setFotoSelecionada({
+                ...fotoSelecionada,
+                favorita: !fotoSelecionada.favorita
+            })
+        }
+        setFotosDaGaleria(fotosDaGaleria.map(fotoDaGaleria => {
+            return {
+                ...fotoDaGaleria,
+                favorita: fotoDaGaleria.id === foto.id ? !foto.favorita : fotoDaGaleria.favorita
+            }
+        }))
+    }
+
+    return (
+        <FundoGradiente>
+            <GlobalStyles />
+            <AppContainer>
+                <Cabecalho />
+                <MainContainer>
+                    <BarraLateral />
+                    <ConteudoGaleria>
+                        <Banner 
+                            texto="A galeria mais completa de fotos do espaço!"
+                            backgroundImage="/images/banner.png"
+                        />
+                        <Galeria 
+                            aoAlternarFavorito={aoAlternarFavorito}
+                            aoFotoSelecionada={foto => setFotoSelecionada(foto)} 
+                            fotos={fotosDaGaleria}
+                        />
+                    </ConteudoGaleria>
+                </MainContainer>
+            </AppContainer>
+            <ModalZoom 
+                foto={fotoSelecionada} 
+                aoFechar={() => setFotoSelecionada(null)}
+                aoAlternarFavorito={aoAlternarFavorito}
+            />
+            <Rodape />
+        </FundoGradiente>
+    )
+}
+
+export default App
